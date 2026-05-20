5a53817938575eaa9b6206b0890a70598be5eaf3
Dennis <hennryasecas@gmail.com>
2026-05-21 00:29:12 +0200

    Archivos FXML de inicio de sesión y registro acabados

---
+++ MapaDemo/src/mapademo/AdjuntosFXML/VistaLoginController.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package mapademo.AdjuntosFXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author dennis
+ */
+public class VistaLoginController implements Initializable {
+
+    @FXML
+    private TextField Nickname;
+    @FXML
+    private PasswordField Password;
+    @FXML
+    private Button Buttom;
+    @FXML
+    private Hyperlink link;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
